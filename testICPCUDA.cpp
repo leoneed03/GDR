cc2a4d4b18579999f8736d23e512f0eaf36adeb6
leoneed03 <mr.senukov@mail.ru>
2020-12-13 04:34:03 +0300

    try without gtsam

--- testICPCUDA.cpp
+++ testICPCUDA.cpp
@@ -6,7 +6,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-#include "include/CorrespondenceGraph.h"
+//#include "include/CorrespondenceGraph.h"
 
 #include <pangolin/image/image_io.h>
 #include <ICPOdometry.h>
@@ -15,6 +15,6 @@ int main() {
 
     ICPOdometry icpOdom(640, 480, 319.5, 239.5, 528, 528);
     std::cout << "hello";
-    CorrespondenceGraph correspondenceGraph("../data/plantSampled_20/rgb", "../data/plantSampled_20/depth", 525.0, 319.5, 525.0, 239.5);
+//    CorrespondenceGraph correspondenceGraph("../data/plantSampled_20/rgb", "../data/plantSampled_20/depth", 525.0, 319.5, 525.0, 239.5);
     return 0;
 }
