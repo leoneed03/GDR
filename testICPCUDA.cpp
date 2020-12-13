2cf25c3dd2b62fc555c92d346b69af5665e1815f
leoneed03 <mr.senukov@mail.ru>
2020-12-13 06:01:34 +0300

    add pcl_include_dirs

--- testICPCUDA.cpp
+++ testICPCUDA.cpp
@@ -11,10 +11,19 @@
 #include <pangolin/image/image_io.h>
 #include <ICPOdometry.h>
 
-int main() {
+void testCG() {
 
     ICPOdometry icpOdom(640, 480, 319.5, 239.5, 528, 528);
     std::cout << "hello";
     CorrespondenceGraph correspondenceGraph("../data/plantSampled_20/rgb", "../data/plantSampled_20/depth", 525.0, 319.5, 525.0, 239.5);
+
+}
+int main() {
+    std::vector<std::vector<int>> pixels;
+    for (int i = 0; i < 480; ++i) {
+        pixels.push_back({i, i, 60000});
+    }
+    pangolin::TypedImage img = LoadImage(pixels, 640, 480, 640);
+
     return 0;
 }
