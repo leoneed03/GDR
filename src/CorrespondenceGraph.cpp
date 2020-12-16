f71f01b1e08908d65443aaf8759c037a4ab5c756
leoneed03 <mr.senukov@mail.ru>
2020-12-16 22:48:26 +0300

    add SiftGpu as external

--- src/CorrespondenceGraph.cpp
+++ src/CorrespondenceGraph.cpp
@@ -680,13 +680,13 @@ CorrespondenceGraph::CorrespondenceGraph(const std::string &pathToImageDirectory
     tranformationRtMatrices = std::vector<std::vector<transformationRtMatrix>>(imagesD.size());
     std::cout << "Totally read " << imagesRgb.size() << std::endl;
 
-    char *myargv[4] = {"-fo", "-1", "-v", "1"};
-    siftModule.sift.ParseParam(4, myargv);
-    int support = siftModule.sift.CreateContextGL();
+    char *myargv[5] = {"-nogl", "-fo", "-1", "-v", "1"};
+    siftModule.sift.ParseParam(5, myargv);
+//    int support = siftModule.sift.CreateContextGL();
     std::cout << "Checking" << std::endl;
-    if (support != SiftGPU::SIFTGPU_FULL_SUPPORTED) {
-        std::cerr << "SiftGPU is not supported!" << std::endl;
-    }
+//    if (support != SiftGPU::SIFTGPU_FULL_SUPPORTED) {
+//        std::cerr << "SiftGPU is not supported!" << std::endl;
+//    }
 
     boost::timer timer;
 
