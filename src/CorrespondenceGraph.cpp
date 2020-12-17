78c0516ef7f9cf5955021c26c64bdbd05d5de9ff
leoneed03 <mr.senukov@mail.ru>
2020-12-18 01:48:28 +0300

    add cuda as arg for siftgpu

--- src/CorrespondenceGraph.cpp
+++ src/CorrespondenceGraph.cpp
@@ -840,7 +840,7 @@ CorrespondenceGraph::CorrespondenceGraph(const std::string &pathToImageDirectory
     tranformationRtMatrices = std::vector<std::vector<transformationRtMatrix >>(imagesD.size());
     std::cout << "Totally read " << imagesRgb.size() << std::endl;
 
-    char *myargv[5] = {"-nogl", "-fo", "-1", "-v", "1"};
+    char *myargv[5] = {"-cuda", "-fo", "-1", "-v", "1"};
 //   for GLSL:
 //    char *myargv[4] = {"-fo", "-1", "-v", "1"};
     std::cout << "Parse params for sift" << std::endl;
