ea76c92d253b756505222c5331df0734faf913c0
leoneed03 <mr.senukov@mail.ru>
2020-12-21 23:04:26 +0300

    add custom error code enum

---
+++ include/errors.h
@@ -0,0 +1,13 @@
+//
+// Copyright (c) Leonid Seniukov. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+//
+
+#ifndef GLOBALRECONSTRUCTIONRGBD_ERRORS_H
+#define GLOBALRECONSTRUCTIONRGBD_ERRORS_H
+
+enum ERRORS {
+    ERROR_OPENING_FILE_WRITE
+};
+
+#endif
