a096cb1ef71bf55e838cdf6e2d4c0441a67c22a1
leoneed03 <mr.senukov@mail.ru>
2020-12-22 04:30:32 +0300

    add lost files

---
+++ include/keyFeatures.h
@@ -0,0 +1,34 @@
+//
+// Copyright (c) Leonid Seniukov. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+//
+
+#ifndef TEST_SIFTGPU_FEATURES_H
+#define TEST_SIFTGPU_FEATURES_H
+
+#include <vector>
+#include <SiftGPU.h>
+#include <string>
+#include <iostream>
+
+#include "fileProc.h"
+
+using imageDescriptor = std::pair<std::vector<SiftGPU::SiftKeypoint>, std::vector<float>>;
+
+std::pair<std::vector<SiftGPU::SiftKeypoint>, std::vector<float>> getKeypointsDescriptorsOneImage(
+        SiftGPU &sift,
+        const std::string &pathToTheImage);
+
+std::vector<std::pair<std::vector<SiftGPU::SiftKeypoint>, std::vector<float>>>
+getKeypointsDescriptorsAllImages(
+        SiftGPU
+        &sift,
+        const std::string &pathToTheDirectory
+);
+
+std::vector<std::pair<int, int>>
+getNumbersOfMatchesKeypoints(const imageDescriptor &keysDescriptors1,
+                             const imageDescriptor &keysDescriptors2,
+                             SiftMatchGPU *matcher);
+
+#endif
